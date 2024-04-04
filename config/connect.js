faa5b33b760a33156f9af3af09f2da0230d3a691
nikhilpj <nikhilpj98@gmail.com>
2024-04-04 14:50:32 +0530

    changes in connection code

--- config/connect.js
+++ config/connect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+require("dotenv").config()
 
-const connectDb =mongoose.connect('mongodb://127.0.0.1:27017/lucidgrowth').then(()=>{
+const connectDb =mongoose.connect(process.env.DATABASE_URI).then(()=>{
     console.log('mongodb connected')
 }).catch((e)=>{
     console.log('eroor occured while connecting db',e)
