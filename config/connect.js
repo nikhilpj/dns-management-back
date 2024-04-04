889f28cbd374a1982f30ad679fca94b0dff9abdf
nikhilpj <nikhilpj98@gmail.com>
2024-04-04 16:18:29 +0530

    changed connection code

--- config/connect.js
+++ config/connect.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose')
 require("dotenv").config()
 
-const connectDb =mongoose.connect(process.env.DATABASE_URI).then(()=>{
-    console.log('mongodb connected')
-}).catch((e)=>{
-    console.log('eroor occured while connecting db',e)
-})
+const connectDb=async ()=>{
+    try {
+        await mongoose.connect(process.env.DATABASE_URI,{
+            useUnifiedTopology:true,
+            useNewUrlParser:true
+        })
+    } catch (error) {
+        console.error(error)
+    }
+
+}
 
 module.exports= connectDb
